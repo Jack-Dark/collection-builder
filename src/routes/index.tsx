64072ec7479e464d2fe94d8817720cd30522e53d
jack-dark <decker3d@gmail.com>
2026-03-12 12:11:58 -0700

    feat: connect to neon

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,43 @@ import * as fs from 'node:fs';
 import { createFileRoute, useRouter } from '@tanstack/react-router';
 import { createServerFn } from '@tanstack/react-start';
 
+import { neon } from '@neondatabase/serverless';
+
+export const getData = createServerFn({ method: 'GET' }).handler(async () => {
+  const sql = neon(process.env.DATABASE_URL!);
+  const response = await sql`SELECT version()`;
+
+  return response[0].version;
+});
+
+const Home = () => {
+  const router = useRouter();
+  const state = Route.useLoaderData();
+
+  return (
+    <button
+      type="button"
+      onClick={() => {
+        updateCount({ data: 1 }).then(() => {
+          router.invalidate();
+        });
+      }}
+    >
+      Add 1 to {state}?
+    </button>
+  );
+};
+
+export const Route = createFileRoute('/')({
+  component: Home,
+  loader: async () => {
+    const data = await getData();
+    console.log('🚀 ~ data:', data);
+
+    return await getCount();
+  },
+});
+
 const filePath = 'count.txt';
 
 async function readCount() {
@@ -23,26 +60,3 @@ const updateCount = createServerFn({ method: 'POST' })
     const count = await readCount();
     await fs.promises.writeFile(filePath, `${count + data}`);
   });
-
-export const Route = createFileRoute('/')({
-  component: Home,
-  loader: async () => await getCount(),
-});
-
-function Home() {
-  const router = useRouter();
-  const state = Route.useLoaderData();
-
-  return (
-    <button
-      type="button"
-      onClick={() => {
-        updateCount({ data: 1 }).then(() => {
-          router.invalidate();
-        });
-      }}
-    >
-      Add 1 to {state}?
-    </button>
-  );
-}
