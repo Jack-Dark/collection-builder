cc70f1c7ca6d29600d68806585911bd7b9e942a7
jack-dark <decker3d@gmail.com>
2026-03-13 17:43:23 -0700

    fix: remove tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import tailwindcss from '@tailwindcss/vite';
 import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
@@ -9,7 +8,6 @@ const config = defineConfig({
   plugins: [
     devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
-    tailwindcss(),
     tanstackStart(),
     // react's vite plugin must come after start's vite plugin
     viteReact(),
