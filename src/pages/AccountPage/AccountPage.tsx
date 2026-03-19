d37c059f79aaea43c5b89391c307c0c226575b07
jack-dark <decker3d@gmail.com>
2026-03-18 17:57:31 -0700

    feat: adds placeholder data to account page

--- src/pages/AccountPage/AccountPage.tsx
+++ src/pages/AccountPage/AccountPage.tsx
@@ -1,3 +1,51 @@
+import VerifiedIcon from '@mui/icons-material/Verified';
+import { authClient } from '#/auth/auth-client';
+import { Button } from '#/components/Button';
+
 export const AccountPage = () => {
-  return null;
+  const { data: session } = authClient.useSession();
+  const user = session?.user;
+
+  // TODO - IMPLEMENT RESEND
+
+  return !user ? null : (
+    <div className="grid gap-8">
+      <h1>Account</h1>
+
+      <div className="grid grid-cols-2 gap-2 items-center">
+        <div className="col-span-2">{user.image}</div>
+
+        <p>{user.name}</p>
+        <Button className="justify-self-start" disabled size="sm">
+          Edit
+        </Button>
+
+        <p>
+          {user.email}
+
+          {user.emailVerified && <VerifiedIcon />}
+        </p>
+        <div className="flex items-center gap-2">
+          <Button className="justify-self-start" disabled size="sm">
+            Edit
+          </Button>
+
+          <Button
+            className="justify-self-start"
+            disabled
+            size="sm"
+            variant="secondary"
+          >
+            Resend verification email
+          </Button>
+        </div>
+
+        <div className="col-span-2">
+          <Button className="justify-self-start" disabled size="sm">
+            Update Password
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
