dbf00dc10158d5df5452a477a1faab1423ff15ff
jack-dark <decker3d@gmail.com>
2026-03-23 09:07:29 -0700

    feat: adds resend support

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,7 +1,11 @@
 import { db } from '#/api/db';
+import { configs } from '#/configs';
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { tanstackStartCookies } from 'better-auth/tanstack-start';
+import { Resend } from 'resend';
+
+const resend = new Resend(configs.resendApiKey);
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -9,6 +13,20 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
+  },
+  emailVerification: {
+    sendVerificationEmail: async ({ token, url, user }, request) => {
+      // TODO - UPDATE BACK TO void FROM await
+      const { data, error } = await resend.emails.send({
+        from: 'onboarding@resend.dev',
+        html: `<p>Click the link to <a href="${configs.authUrl}/account?token=${token}">verify your email</a></p>`,
+        subject: 'Hello World',
+        to: 'decker3d@gmail.com',
+      });
+      if (error) {
+        console.log('🚀 ~ error:', error);
+      }
+    },
   },
   plugins: [
     // make sure this is the last plugin in the array
