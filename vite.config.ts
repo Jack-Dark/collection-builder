c4a01c62dfa9d39f4aecd2dc85360a7a763055e1
jack-dark <decker3d@gmail.com>
2026-04-21 10:43:10 -0700

    fix: adds ts validation to the error overlay

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,19 @@ import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 const config = defineConfig({
   plugins: [
     devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    checker({
+      // eslint: {
+      //   lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      // },
+      typescript: true,
+    }),
     tailwindcss(),
     tanstackStart(),
     // react's vite plugin must come after start's vite plugin
