29edb9cfd05f1dc892d76a3498da29bd20973b83
jack-dark <decker3d@gmail.com>
2026-04-29 11:31:20 -0700

    fix: adds schema validation to auth middleware

--- src/auth/auth-middleware.ts
+++ src/auth/auth-middleware.ts
@@ -1,24 +1,42 @@
 import { createMiddleware } from '@tanstack/react-start';
 import { StatusCodes, getReasonPhrase } from 'http-status-codes';
+import z from 'zod';
 
 import { getUserContext } from './auth.functions';
+
+const authSchema = z.object({
+  id: z.string(),
+  image: z.string().nullable().optional(),
+  name: z.string(),
+  token: z.string(),
+});
+
+const unauthorizedMsg = getReasonPhrase(StatusCodes.UNAUTHORIZED);
 
 /** Use this middleware to authenticate protected API routes. */
 export const authApiRouteMiddleware = createMiddleware().server(
   async ({ next }) => {
-    const user = await getUserContext();
+    const userContext = await getUserContext();
 
-    if (!user) {
+    if (!userContext) {
       return new Response(
-        JSON.stringify({ error: getReasonPhrase(StatusCodes.UNAUTHORIZED) }),
+        JSON.stringify({ error: { message: unauthorizedMsg } }),
         { status: StatusCodes.UNAUTHORIZED },
       );
     }
 
-    return await next({
-      context: {
-        user,
-      },
-    });
+    const { data, error, success } = z.safeParse(authSchema, userContext);
+
+    if (success) {
+      return await next({
+        context: {
+          user: data,
+        },
+      });
+    } else {
+      return new Response(JSON.stringify({ error }), {
+        status: StatusCodes.UNPROCESSABLE_ENTITY,
+      });
+    }
   },
 );
